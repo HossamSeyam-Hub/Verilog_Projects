026b7403d869c975d44cdfafab3e14c1e5c5d17e
Hossam Seyam <hossam.a.seyam@gmail.com>
2024-11-04 14:37:32 -0800

    Add files via upload

---
+++ 1-Simple/2-Data_flow/mux_2to1_df1.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+/*Hossam Ahmed Seyam*/
+/*This code is illustrating How to design 2*1 mux in structural modelling especially data flow modeling */
+
+module mux_2to1_df1(x1, x2, s, f);
+
+	input wire x1, x2, s;	// x1,x2 are mux inputs , s is the selector of the 2*1 mux
+	output wire f;			//mux output
+	
+	assign f = (x1 & ~s)|(x2 & s); //f =( x1.not(s) +  x2.s )
+
+endmodule
